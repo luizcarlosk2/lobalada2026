c1e9ee0dc0410e9abdd395cbbda121ad509ef1f5
Luiz Franze <luizcarlos.k2@gmail.com>
2026-04-14 20:35:51 -0300

    Bugfix: precos com ','

---
+++ config.js
@@ -0,0 +1,9 @@
+const CONFIG = {
+
+  // Fonte dos dados: 'local' para precos.csv do repositório, 'google' para Google Sheets
+  fonte: 'google',
+
+  csv_local: 'precos.csv',
+  csv_google: 'https://docs.google.com/spreadsheets/d/e/2PACX-1vRKA_ZiwZHFY6rqrONsMyB4iIw2O6lp0B4bRmN5Os9a8osIefdZzsKPVWk7TfMk-8GSOGD7kyzqy7sD/pub?output=csv'
+
+};
