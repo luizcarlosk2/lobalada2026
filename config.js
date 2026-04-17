eab36b420da4efc11785dc0141e9cabb11792636
Luiz Franze <luizcarlos.k2@gmail.com>
2026-04-17 20:24:17 -0300

    Impr: Version e Price version

--- config.js
+++ config.js
@@ -1,10 +1,12 @@
 const CONFIG = {
 
+  // Versão do app (HTML)
+  versao_app: '0.3.0',
+
   // Fonte dos dados: 'local' para precos.csv do repositório, 'google' para Google Sheets
   fonte: 'google',
 
   csv_local: 'precos.csv',
-  // csv_google: 'https://docs.google.com/spreadsheets/d/e/2PACX-1vRKA_ZiwZHFY6rqrONsMyB4iIw2O6lp0B4bRmN5Os9a8osIefdZzsKPVWk7TfMk-8GSOGD7kyzqy7sD/pub?output=csv'
-     csv_google: 'https://docs.google.com/spreadsheets/d/e/1vRKA_ZiwZHFY6rqrONsMyB4iIw2O6lp0B4bRmN5Os9a8osIefdZzsKPVWk7TfMk-8GSOGD7kyzqy7sD/pub?output=csv'
+  csv_google: 'https://docs.google.com/spreadsheets/d/e/2PACX-1vRKA_ZiwZHFY6rqrONsMyB4iIw2O6lp0B4bRmN5Os9a8osIefdZzsKPVWk7TfMk-8GSOGD7kyzqy7sD/pub?output=csv'
 
 };
