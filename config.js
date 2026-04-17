fcc5e58f44fbd24f4946273f34e98430d56d45e6
Luiz Franze <luizcarlos.k2@gmail.com>
2026-04-17 20:16:30 -0300

    Test: Teste de fallback

--- config.js
+++ config.js
@@ -4,6 +4,7 @@ const CONFIG = {
   fonte: 'google',
 
   csv_local: 'precos.csv',
-  csv_google: 'https://ERROdocs.google.com/spreadsheets/d/e/2PACX-1vRKA_ZiwZHFY6rqrONsMyB4iIw2O6lp0B4bRmN5Os9a8osIefdZzsKPVWk7TfMk-8GSOGD7kyzqy7sD/pub?output=csv'
+  // csv_google: 'https://docs.google.com/spreadsheets/d/e/2PACX-1vRKA_ZiwZHFY6rqrONsMyB4iIw2O6lp0B4bRmN5Os9a8osIefdZzsKPVWk7TfMk-8GSOGD7kyzqy7sD/pub?output=csv'
+     csv_google: 'https://docs.google.com/spreadsheets/d/e/1vRKA_ZiwZHFY6rqrONsMyB4iIw2O6lp0B4bRmN5Os9a8osIefdZzsKPVWk7TfMk-8GSOGD7kyzqy7sD/pub?output=csv'
 
 };
